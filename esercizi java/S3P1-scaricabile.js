94eccc38c89b657094c4669614b5f2852e5083f4
PaulDimson <148753327+PaulDimson@users.noreply.github.com>
2023-11-22 19:41:49 +0100

    Add files via upload

---
+++ esercizi java/S3P1-scaricabile.js
@@ -0,0 +1,103 @@
+/* ESERCIZIO 1
+ Scrivi una funzione chiamata "crazySum" che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma di quei due valori, ma se il loro valore è lo stesso allora deve ritornare la loro somma moltiplicata per 3.
+*/
+
+ function crazySum (firstNum,secondNum) {
+    let sumResult = firstNum + secondNum;
+    if (firstNum === secondNum) {
+        sumResult = (firstNum + secondNum)* 3;
+    }
+    return sumResult;
+ }
+
+ let finalResult = crazySum (7, 9);
+ console.log (finalResult);
+
+/* ESERCIZIO 2
+ Scrivi una funzione chiamata "boundary", che accetta un numero intero come parametro e ritorna true se tale parametro è incluso tra 20 e 100 (incluso) o se è esattamente uguale a 400.
+*/
+
+function boundary (x) {
+    if ((x >= 20 && x <= 100) || x === 400) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let boundNum = boundary (30);
+console.log (boundNum); // true
+
+boundNum = boundary (400);
+console.log (boundNum); // true
+
+boundNum = boundary (350);
+console.log (boundNum); // false
+
+/* ESERCIZIO 3
+ Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e la ritorna invertita (es.: EPICODE => EDOCIPE).
+*/
+
+function reverseString (string) {
+    return string.split("").reverse().join("");
+}
+let myReverseString = reverseString ("EPICODE");
+console.log (myRevesreString);
+
+/* ESERCIZIO 4
+ Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.
+*/
+
+function upperFirst (string) {
+    let parole = string.split(" ");
+
+    for(let i = 0; i < parole.lenght; i++) {
+        parole[i] = parole [i][0].toUpperCase() +parole[i].substring(1);
+    }
+
+    return parole.join(" ");
+}
+
+console.log(upperFirst("I love fish"))
+
+/* ESERCIZIO 5
+ Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+//EXTRA:
+/* ESERCIZIO 1
+ Scrivi una funzione chiamata "area" che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 2
+ Scrivi una funzione chiamata "crazyDiff" che calcola la differenza assoluta tra un numero fornito e 19.
+ Se il valore calcolato è più grande di 19, la funzione deve tornare tale risultato moltiplicato per 3.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+ Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "code" all'inizio della stringa fornita e ritornare il risultato, ma se la stringa fornita comincia proprio con "code" allora deve ritornarla senza modifiche.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
+ La funzione deve controllare che tale parametro sia un multiplo di 3 o di 7, e in tal caso tornare true; altrimenti deve tornare false.
+ SUGGERIMENTO: operatore modulo
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 5
+ Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
